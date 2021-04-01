6818c9830603aaa4c8e44a529c6c0f79d39fe3db
Bharath-Kbpr <iamkbpr@gmail.com>
2021-04-01 14:54:14 +0530

    1commit

---
+++ form_validation/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Title from './components/Title'
+import Form from './components/Form'
+
+function App() {
+  return (
+    <div className="App">
+   <Title />
+   <Form />
+    </div>
+  );
+}
+
+export default App;
---
+++ form_validation/src/components/Title.js
@@ -0,0 +1,11 @@
+
+const Title = () => {
+    return (
+        <header class="title">
+           <h1>Form Valiation<span style={{color:'red'}}>*</span></h1>
+           
+        </header>
+    )
+}
+
+export default Title
