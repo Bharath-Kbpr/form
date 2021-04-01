9bbb579bd67dca303c618ddae74cc7fea2a55674
Mano Sriram <mano.sriram0@gmail.com>
2021-04-01 15:52:18 +0530

    Add store, reducers

--- form_validation/src/components/Form.js
+++ form_validation/src/components/Form.js
@@ -1,24 +1,107 @@
+import { connect } from "react-redux";
+import * as actionCreator from "../store/actions/index";
 
-const Form = () => {
-
+const Form = props => {
     return (
-        <form>
-        <input className="row1" type="text"   placeholder="Associate Name" /><br />
-        <input className="row2" type="text"   placeholder="Associate Id"/><br />
-        <input className="row3" type="text" placeholder="Project ID" /><br />
-        <input className="row4" type="radio" value="onshore" name="shore" /><label >onshore</label>
-        <input className="row5" type="radio" value="offshore" name="shore"/><label >offshore</label><br />
-        <select className="row6">
-        <option disabled selected value="">Please select</option>
-        </select><br />
-        <input className="row7" type="checkbox"/>
-                <label >HTML</label><br />
-                <input className="row8" type="file" id="img" name="img" accept="image/*" /><br />
-                <textarea className="row9" placeholder='Comments'></textarea><br />
-                <button class="submit"  style={{backgroundColor:'#3398FF ',padding:'12px',outline:'none',border:'none',borderRadius:"5px",color:'#ffffff',margin:"0px 5px 0px 0px"}}>Submit</button>
-            <button class="reset" style={{backgroundColor:'#FF3355 ',padding:'12px',outline:'none',border:'none',borderRadius:"5px",color:'#ffffff'}}>Reset</button>
+        <form onSubmit={props.handleSubmit} onChange={props.handleChange}>
+            <input
+                className="row1"
+                type="text"
+                name="name"
+                placeholder="Associate Name"
+            />
+            <br />
+            <input
+                className="row2"
+                type="text"
+                name="id"
+                placeholder="Associate Id"
+            />
+            {props.error && (
+                <>
+                    <br />
+                    <label for="">{props.error}</label>
+                </>
+            )}
+            <br />
+            <input
+                className="row3"
+                type="text"
+                name="pid"
+                placeholder="Project ID"
+            />
+            <br />
+            <input className="row4" type="radio" value="onshore" name="shore" />
+            <label>onshore</label>
+            <input
+                className="row5"
+                type="radio"
+                value="offshore"
+                name="shore"
+            />
+            <label>offshore</label>
+            <br />
+            <select className="row6">
+                <option disabled selected value="">
+                    Please select
+                </option>
+            </select>
+            <br />
+            <input className="row7" type="checkbox" />
+            <label>HTML</label>
+            <br />
+            <input
+                className="row8"
+                type="file"
+                id="img"
+                name="img"
+                accept="image/*"
+            />
+            <br />
+            <textarea className="row9" placeholder="Comments"></textarea>
+            <br />
+            <button
+                class="submit"
+                style={{
+                    backgroundColor: "#3398FF ",
+                    padding: "12px",
+                    outline: "none",
+                    border: "none",
+                    borderRadius: "5px",
+                    color: "#ffffff",
+                    margin: "0px 5px 0px 0px"
+                }}
+            >
+                Submit
+            </button>
+            <button
+                class="reset"
+                style={{
+                    backgroundColor: "#FF3355 ",
+                    padding: "12px",
+                    outline: "none",
+                    border: "none",
+                    borderRadius: "5px",
+                    color: "#ffffff"
+                }}
+            >
+                Reset
+            </button>
         </form>
-    )
-}
+    );
+};
 
-export default Form
+const mapStateToProps = state => {
+    return {
+        error: state.error
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        handleChange: e => dispatch(actionCreator.handleChange(e)),
+        handleSubmit: e => dispatch(actionCreator.handleSubmit(e))
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Form);
---
+++ form_validation/src/store/actions/index.js
@@ -0,0 +1,13 @@
+export const handleChange = e => {
+    return {
+        type: "CHANGE",
+        e
+    };
+};
+
+export const handleSubmit = e => {
+    return {
+        type: "SUBMIT",
+        e
+    };
+};
